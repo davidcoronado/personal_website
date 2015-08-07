07557888ca3cf9a7c06ee63a9c29b3b60503e92a
davidcoronado <davidjcoronado@gmail.com>
2015-08-07 13:38:05 -0700

    Updated controller view and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "pages#home"
+  get "portfolio" => "pages#portfolio"
+  get "video" => "pages#video"
+  get "photo" => "pages#photo"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
